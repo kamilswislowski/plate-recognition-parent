1fca21ff7a62650c8850314997c86fc5c1401339
Kamil Swislowski <swislowski.kamil@gmail.com>
2019-12-03 12:20:16 +0100

    Added Logger in Controller

--- src/main/java/pl/swislowski/kamil/project/platerecognition/spring/web/controller/RecognitionRegistrationPlateController.java
+++ src/main/java/pl/swislowski/kamil/project/platerecognition/spring/web/controller/RecognitionRegistrationPlateController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.swislowski.kamil.project.platerecognition.spring.web.model.RegistrationPlateModel;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping(value = "/plate-recognition")
 public class RecognitionRegistrationPlateController {
+    private static final Logger LOGGER = Logger.getLogger(RecognitionRegistrationPlateController.class.getName());
 
     @PostMapping(value = "/recognize")
     public RegistrationPlateModel recognize() {
+        LOGGER.info("Recognizing ...");
         return new RegistrationPlateModel("WI 234212");
     }
 }
